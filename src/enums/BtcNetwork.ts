6e5a6bf45c10ecfe386c6f8e7e20904c226414c3
Hyunhum <hyeonheum@naver.com>
2022-11-03 11:20:22 +0900

    feat: add liquid testnet support

--- src/enums/BtcNetwork.ts
+++ src/enums/BtcNetwork.ts
@@ -1,7 +1,8 @@
 enum BtcNetwork {
     Mainnet = "mainnet",
     Testnet = "testnet",
-    Liquid = "liquid"
+    Liquid = "liquid",
+    Liquid_Testnet = "liquid testnet"
 }
 
 export default BtcNetwork;
