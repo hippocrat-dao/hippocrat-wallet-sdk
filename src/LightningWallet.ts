b5ae7acc7071bb50fff76d894ead5d50124eae97
Hyunhum <hyeonheum@naver.com>
2022-11-10 14:58:06 +0900

    feat: apply type to Lightning Wallet module

--- src/LightningWallet.ts
+++ src/LightningWallet.ts
@@ -4,40 +4,46 @@ import LightningAuth from './enums/LightningAuth.js'
 class LightningWallet {
 
   static authenticateSigner = async ()
-  :Promise<any> => {
-    const authResult : any = lightning.authenticatedLndGrpc({
+  :Promise<lightning.AuthenticatedLnd> => {
+    const { lnd } = lightning.authenticatedLndGrpc({
       cert: LightningAuth.TLS,
       macaroon: LightningAuth.Macaroon,
-      socket: '127.0.0.1:10009',
+      socket: LightningAuth.Socket,
     });
-
-    return authResult.lnd;
+    // lnd is necessry arg for most of methods
+    return lnd as lightning.AuthenticatedLnd;
   }
 
-  static getWalletInfo = async (lnd: any)
-  :Promise<any> => {
-    const wallet : any = await lightning.getWalletInfo({lnd});
-
+  static getWalletInfo = async (lnd: lightning.AuthenticatedLnd)
+  :Promise<lightning.GetWalletInfoResult> => {
+    const wallet : lightning.GetWalletInfoResult = await lightning.getWalletInfo({lnd});
+    // wallet of lnd
     return wallet;
   }
 
-  static createInvoice = async (lnd: any)
-  :Promise<any> => {
-    const invoice : any = await lightning.createInvoice({lnd});
-
+  static createInvoice = async (lnd: lightning.AuthenticatedLnd)
+  :Promise<lightning.CreateInvoiceResult> => {
+    /*
+      there's no "address" in lightning network
+      only way to transfer is by creating invoice,
+      which expires in 72 hours
+    */
+    const invoice : lightning.CreateInvoiceResult = await lightning.createInvoice({lnd});
+    // invoice to show client
     return invoice;
   }
 
   static createChannel =  async (
-    lnd:any , publicKey: string, channelSize: number)
-  :Promise<any> => {
+    lnd: lightning.AuthenticatedLnd, publicKey: string, channelSize: number)
+  :Promise<lightning.OpenChannelResult> => {
     /*
       MyPublicKey = "029a566b8195283ebf34b10ee3e4b687ab618c1a7856a29dd58ba12a63abba7518";
       channelSize must be >= 1000000;
     */
-    const res =  lightning.openChannel({lnd, local_tokens: channelSize, partner_public_key: publicKey});
+    const channel : lightning.OpenChannelResult = await lightning.openChannel(
+      {lnd, local_tokens: channelSize, partner_public_key: publicKey});
 
-    return res;
+    return channel;
   }
 
 }
