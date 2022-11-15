8a9e20139594a58270bed93a11057a05f5fffa2c
Hyunhum <hyeonheum@naver.com>
2022-11-15 16:42:21 +0900

    test: add btc address generator test

--- test/BtcWallet.spec.ts
+++ test/BtcWallet.spec.ts
@@ -2,6 +2,7 @@ import * as assert from "assert";
 import { BIP32Interface } from "bip32";
 import { describe, it } from 'mocha';
 import BtcWallet from "../src/BtcWallet.js";
+import BtcNetwork from "../src/enums/BtcNetwork.js";
 
 describe('Mnemonic generator test', () => {
     it('should return 12 words mnemonic', async() => {
@@ -27,5 +28,18 @@ describe('Grandchild BIP32 generator test', () => {
         const btcAccountPotentialChild : BIP32Interface = await BtcWallet.getChildFromAccount(btcAccountPotentialParent);
         const privateKey : string = (btcAccountPotentialChild.privateKey as Buffer).toString('hex');
         assert.strictEqual(privateKey.length, 64);
+    })
+})
+
+describe('Btc address generator test', () => {
+    it('should return segwit address with 21 bytes hex with prefix "bc1"', async() => {
+        const mnemonic : string = await BtcWallet.generateWalletMnemonic();
+        const btcAccountPotential : BIP32Interface = await BtcWallet.getChildFromMnemonic(mnemonic);
+        const btcNetwork : BtcNetwork = BtcNetwork.Mainnet;
+        const btcAddress : string = await BtcWallet.generateBtcAddressFromAccount(
+            btcAccountPotential, btcNetwork);
+
+        assert.strictEqual(btcAddress.length, 42);
+        assert.strictEqual(btcAddress.slice(0, 3), "bc1");
     })
 })
