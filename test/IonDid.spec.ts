d85589910055c7e543776b2290315b18ac1f4e74
Hyunhum <hyeonheum@naver.com>
2022-11-17 16:01:05 +0900

    test: add ionDid getUri method test

--- test/IonDid.spec.ts
+++ test/IonDid.spec.ts
@@ -58,3 +58,39 @@ describe('create ION DID test', () => {
 
     })
 })
+
+describe('create ION DID test', () => {
+    it('check Ion DID format', async() => {
+
+        const mnemonic : string = await BtcWallet.generateWalletMnemonic();
+        const btcAccountPotentialParent : BIP32Interface = await BtcWallet.getChildFromMnemonic(mnemonic);
+        const ionAccountPotentialChild : BIP32Interface = await BtcWallet.getChildFromAccount(btcAccountPotentialParent);        
+        const ionJwkPair : JsonWebKey2020 = await IonDid.generateKeyPair(ionAccountPotentialChild);
+        const ionServices : IonService[] = [
+            {
+                id: "tb1qyk6e26ey6v0qc6uaxl9h3ky86uek3qhwx7pq3c",
+                type: "hipocrat patient wallet",
+                serviceEndpoint: "https://blockstream.info/testnet/address/tb1qsww2x6w2mjmdfv3lcr6gxzxfalykrxxsqprpp7"
+            },
+            {
+                id: "tb1qsww2x6w2mjmdfv3lcr6gxzxfalykrxxsqprpp7",
+                type: "hipocrat admin wallet",
+                serviceEndpoint: "https://blockstream.info/testnet/address/tb1qyk6e26ey6v0qc6uaxl9h3ky86uek3qhwx7pq3c"
+            },
+            {
+                id: "hipocrat0patient0id",
+                type: "hipocrat patient data",
+                serviceEndpoint: "https://ipfs.io/ipfs/bafybeialzikkahbkmyjegm5tgus3m4izqntr647pf7uqvhglossdop5fau"
+            }
+        ]
+        const IonDID : IonDidModel = await IonDid.createDid(ionJwkPair, ionServices);
+
+        const ionDidUriShort : string = await IonDid.getDidUriShort(IonDID);
+        const ionDidUriLong : string = await IonDid.getDidUriLong(IonDID);
+
+        assert.strictEqual(ionDidUriShort.slice(0,8), 'did:ion:');
+        assert.strictEqual(ionDidUriLong.slice(0,8), 'did:ion:');
+        assert.strictEqual(ionDidUriShort, ionDidUriLong.slice(0,54));
+
+    })
+})
