6756e9ab0856e6c1766e661dd3d2dc14f7439fd8
Hyunhum <hyeonheum@naver.com>
2022-11-28 17:36:23 +0900

    feat: add interface of properties for BtcSigner model

--- sdk/models/BtcSigner.ts
+++ sdk/models/BtcSigner.ts
@@ -1,7 +1,36 @@
-import * as bitcoin from 'bitcoinjs-lib';
-import * as ecPair from 'ecpair';
+import {Network, Signer} from './BtcAccount';
+
+interface Payment {
+    name?: string;
+    network?: Network;
+    output?: Buffer;
+    data?: Buffer[];
+    m?: number;
+    n?: number;
+    pubkeys?: Buffer[];
+    input?: Buffer;
+    signatures?: Buffer[];
+    pubkey?: Buffer;
+    signature?: Buffer;
+    address?: string;
+    hash?: Buffer;
+    redeem?: Payment;
+    witness?: Buffer[];
+}
+
+interface ECPairInterface extends Signer {
+    compressed: boolean;
+    network: Network;
+    lowR: boolean;
+    privateKey?: Buffer;
+    toWIF(): string;
+    tweak(t: Buffer): ECPairInterface;
+    verify(hash: Buffer, signature: Buffer): boolean;
+    verifySchnorr(hash: Buffer, signature: Buffer): boolean;
+    signSchnorr(hash: Buffer): Buffer;
+}
 
 export default interface BtcSigner {
-    payment: bitcoin.payments.Payment,
-    keyPair: ecPair.ECPairInterface
+    payment: Payment,
+    keyPair: ECPairInterface
 }
