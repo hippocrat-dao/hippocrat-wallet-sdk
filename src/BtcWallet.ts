81a09eb687047befc9cd17e974da9b130f5f3a4b
Hyunhum <hyeonheum@naver.com>
2023-01-09 15:56:46 +0900

    feat: add geNonBtcAccountFromMnemonic for various purpose

--- src/BtcWallet.ts
+++ src/BtcWallet.ts
@@ -54,6 +54,28 @@ class BtcWallet{
         // child has 32 bytes private key, used for btc wallet private key directly
         return child;
     }
+        /* 
+    Used for various purpose(IonDid, RareData) where EC is required,
+    sepeated from BTC for security reasons while using same mnemoinc.
+    Directly get non-btc purpose account from mnemoinc easily!
+    */
+    static getNonBtcAccountFromMnemonic = async (mnemonic : string, purpose = 0, index = 0)
+    : Promise<BtcAccount> => {
+        const seed : Buffer = bip39.mnemonicToSeedSync(mnemonic);
+        // hd wallet(master wallet)
+        const root : bip32.BIP32Interface = bip32.BIP32Factory(
+            ecc as bip32.TinySecp256k1Interface).fromSeed(seed)
+        const child : bip32.BIP32Interface = root
+            .deriveHardened(44 as number) // purpose
+            .derive(0 as number) // coin type
+            .derive(0 as number) // account
+            .derive(0 as number) // for external use only
+            .derive(0 as number) // address to use
+            .derive(purpose as number) // ex) ION = 0, RareData = 1
+            .derive(index as number); // account to use
+        // child has 32 bytes private key of EC, used for various purposes
+        return child;
+    }
     // Account is compressed pubkey in BTC network
     static generateBtcAddress = async (
         btcAccountPotential : BtcAccount,
