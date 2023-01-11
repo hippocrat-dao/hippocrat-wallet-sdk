7b515ba4b16bf063bdd1cf25bbc08cd728dca165
Hyunhum <hyeonheum@naver.com>
2023-01-11 17:15:20 +0900

    refactor: change LightningAuth from enum to interface

--- index.ts
+++ index.ts
@@ -6,7 +6,6 @@ import RareData from "./src/RareData.js";
 import LightningPayment from "./src/LightningPayment.js";
 import BtcNetwork from "./src/enums/BtcNetwork.js";
 import BtcRpcUrl from "./src/enums/BtcRpcUrl.js";
-import LightningAuth from "./src/enums/LightningAuth.js";
 import TxFee from "./src/enums/TxFee.js";
 import BtcAccount from "./src/models/BtcAccount.js";
 import BtcReceiver from "./src/models/BtcReceiver.js";
@@ -18,8 +17,9 @@ import IonService from "./src/models/IonService.js";
 import IonKeyPair from "./src/models/IonKeyPair.js";
 import JsonWebKey2020 from "./src/models/JsonWebKey2020.js";
 import UTXO from "./src/models/UTXO.js";
+import LightningAuth from "./src/models/LightningAuth.js";
 
 export { BtcPayment, BtcWallet, BtcRpcNode, IonDid, RareData, LightningPayment };
-export { BtcNetwork, BtcRpcUrl, LightningAuth, TxFee };
+export { BtcNetwork, BtcRpcUrl, TxFee };
 export { BtcAccount, BtcReceiver, BtcSigner, ECIES, IonDidModel, 
-    IonDidResolved, IonService, IonKeyPair, JsonWebKey2020, UTXO};
+    IonDidResolved, IonService, IonKeyPair, JsonWebKey2020, UTXO, LightningAuth};
