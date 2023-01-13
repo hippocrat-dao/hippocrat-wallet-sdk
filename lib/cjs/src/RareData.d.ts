2c5764b49168de3b55b4f19184719ec61374c901
Hyunhum <hyeonheum@naver.com>
2023-01-13 17:20:30 +0900

    feat: update compiled and browser lib as module updated

--- lib/cjs/src/RareData.d.ts
+++ lib/cjs/src/RareData.d.ts
@@ -1,7 +1,6 @@
-import ECIES from './models/ECIES';
 declare class RareData {
-    static encryptData: (publicKeyHexTo: string, data: string) => Promise<ECIES>;
-    static decryptData: (privateKeyHex: string, encryptedData: ECIES) => Promise<string>;
+    static encryptData: (publicKeyHexTo: string, data: string) => Promise<string>;
+    static decryptData: (privateKeyHex: string, encryptedData: string) => Promise<string>;
     static encryptDataShared: (privateKeyHexA: string, publicKeyHexB: string, data: string) => Promise<string>;
     static decryptDataShared: (privateKeyHexB: string, publicKeyHexA: string, encryptedDataSharedStr: string) => Promise<string>;
 }
--- lib/esm/src/BtcPayment.d.ts
+++ lib/esm/src/BtcPayment.d.ts
@@ -3,7 +3,7 @@ import BtcReceiver from './models/BtcReceiver.js';
 import BtcNetwork from './enums/BtcNetwork.js';
 import TxFee from './enums/TxFee.js';
 declare class BtcPayment {
-    static getBtcSigner: (mnemonic: string, btcNetwork: BtcNetwork, accountIndex?: number, addressIndex?: number, addressReuse?: boolean) => Promise<BtcSigner>;
+    static getBtcSigner: (mnemonic: string, btcNetwork?: BtcNetwork, accountIndex?: number, addressIndex?: number, addressReuse?: boolean) => Promise<BtcSigner>;
     static writeOnBtc: (signer: BtcSigner, messageList: string[], txFee: TxFee) => Promise<string>;
     static transferBtc: (signer: BtcSigner, receiverList: BtcReceiver[], txFee: TxFee) => Promise<string>;
     private static _utxoOptimizer;
