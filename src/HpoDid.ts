a0cc9cfbc78556203be34849c196dbd600304b08
Hyunhum <hyeonheum@naver.com>
2023-02-24 18:54:14 +0900

    feat: add HpoDid Module

---
+++ src/HpoDid.ts
@@ -0,0 +1,28 @@
+import BtcWallet from './BtcWallet';
+import BtcAccount from './models/BtcAccount';
+import HpoDidModel from './models/HpoDidModel';
+
+class HpoDid{
+    // derive HPO DID account from BTC HD wallet mnemonic
+    static createDid = async (mnemonic : string, purpose = 0, index = 0)
+    : Promise<HpoDidModel> => {
+        // HpoDid to generate document
+        const HpoDid : BtcAccount = await BtcWallet.getNonBtcAccountFromMnemonic(mnemonic, purpose, index);
+        // HpoDid document
+        const HpoDidDocument : HpoDidModel = {
+            '@context': 'https://www.w3.org/ns/did/v1',
+            'id': 'did:hpo:' + HpoDid.publicKey.toString('hex'),
+            'verificationMethod': [
+                {
+                    'id': 'did:hpo:' + HpoDid.publicKey.toString('hex'),
+                    'type': 'did:hpo:' + 'EcdsaSecp256k1VerificationKey2019' as 'EcdsaSecp256k1VerificationKey2019',
+                    'controller': 'did:hpo:' + HpoDid.publicKey.toString('hex'),
+                    'publicKeyHex': 'did:hpo:' + HpoDid.publicKey.toString('hex')
+                }
+            ]
+        }
+        return HpoDidDocument;
+    }
+}
+
+export default HpoDid;
