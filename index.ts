bc1cc802a4cf56e9883436e98d3a2787de9573ec
ChrisCho-H <hyeonheum@naver.com>
2023-08-09 16:55:51 +0900

    feat: add HpoPayment module to transfer HPO

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import BtcRpcNode from './src/BtcRpcNode.js';
 import RareData from './src/RareData.js';
 import LNPayment from './src/LNPayment.js';
 import HpoDid from './src/HpoDid.js';
+import HpoPayment from './src/HpoPayment.js';
 import BtcNetwork from './src/enums/BtcNetwork.js';
 import BtcRpcUrl from './src/enums/BtcRpcUrl.js';
 import TxFee from './src/enums/TxFee.js';
@@ -14,6 +15,14 @@ import UTXO from './src/models/UTXO.js';
 import BOLT11 from './src/models/BOLT11.js';
 import HpoDidModel from './src/models/HpoDidModel.js';
 
-export { BtcPayment, BtcWallet, BtcRpcNode, RareData, LNPayment, HpoDid };
+export {
+	BtcPayment,
+	BtcWallet,
+	BtcRpcNode,
+	RareData,
+	LNPayment,
+	HpoDid,
+	HpoPayment,
+};
 export { BtcNetwork, BtcRpcUrl, TxFee };
 export { BtcAccount, BtcReceiver, BtcSigner, UTXO, BOLT11, HpoDidModel };
