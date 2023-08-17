ea7dc159d8da4938e966fb720b2f5aa4fea857b9
ChrisCho-H <hyeonheum@naver.com>
2023-08-17 18:05:40 +0900

    refactor: remove LNPayment module in index.ts

--- index.ts
+++ index.ts
@@ -2,7 +2,6 @@ import BtcPayment from './src/BtcPayment.js';
 import BtcWallet from './src/BtcWallet.js';
 import BtcRpcNode from './src/BtcRpcNode.js';
 import RareData from './src/RareData.js';
-import LNPayment from './src/LNPayment.js';
 import HpoDid from './src/HpoDid.js';
 import HpoPayment from './src/HpoPayment.js';
 import BtcNetwork from './src/enums/BtcNetwork.js';
@@ -16,14 +15,6 @@ import UTXO from './src/models/UTXO.js';
 import BOLT11 from './src/models/BOLT11.js';
 import HpoDidModel from './src/models/HpoDidModel.js';
 
-export {
-	BtcPayment,
-	BtcWallet,
-	BtcRpcNode,
-	RareData,
-	LNPayment,
-	HpoDid,
-	HpoPayment,
-};
+export { BtcPayment, BtcWallet, BtcRpcNode, RareData, HpoDid, HpoPayment };
 export { BtcNetwork, BtcRpcUrl, TxFee, MnemonicLang };
 export { BtcAccount, BtcReceiver, BtcSigner, UTXO, BOLT11, HpoDidModel };
